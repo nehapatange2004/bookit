102d9071ba1680d0c1e59671b3a9a8e57da9a5de
nehapatange2004 <nehapatange2004@gmail.com>
2025-10-29 18:10:00 +0530

    added baseurl when no env is there

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://bookit-yoj8.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
